3b707aae8c7aa7676562f5aba65d92c2c8cb2a68
maccarif <fernando.maccari@barrons.com>
2024-12-05 10:24:01 +0200

    feat: add input management

--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,4 @@
-const checkboxes: Record<string, boolean> = {};
+const checkboxes: Record<string, { checked: boolean; inputs: string[] }> = {};
 
 document.addEventListener("DOMContentLoaded", () => {
   const checkboxIds = [
@@ -12,9 +12,17 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector<HTMLInputElement>(`#${id}`),
   );
 
+  const inputElements = checkboxIds.map((id, index) => [
+    document.querySelector<HTMLInputElement>(`#paramHeader${index + 1}`),
+    document.querySelector<HTMLInputElement>(`#paramName${index + 1}`),
+  ]);
+
   // Check if any checkbox is missing
-  if (checkboxElements.some((checkbox) => !checkbox)) {
-    console.error("One or more checkboxes are missing.");
+  if (
+    checkboxElements.some((checkbox) => !checkbox) ||
+    inputElements.some(([inputA, inputB]) => !inputA || !inputB)
+  ) {
+    console.error("One or more elements are missing.");
     return;
   }
 
@@ -25,7 +33,12 @@ document.addEventListener("DOMContentLoaded", () => {
       checkboxElements.forEach((checkbox, index) => {
         if (!checkbox) return;
         const key = `checkbox${index + 1}`;
-        checkbox.checked = checkboxes[key] || false;
+        const [inputA, inputB] = inputElements[index];
+        checkbox.checked = checkboxes[key]?.checked || false;
+        if (inputA && inputB) {
+          inputA.value = checkboxes[key]?.inputs?.[0] || "";
+          inputB.value = checkboxes[key]?.inputs?.[1] || "";
+        }
       });
     }
   });
@@ -41,16 +54,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // Add event listeners for all checkboxes
+  // Handle changes to checkboxes and inputs
   checkboxElements.forEach((checkbox, index) => {
-    if (!checkbox) return;
     const key = `checkbox${index + 1}`;
+    const [inputA, inputB] = inputElements[index];
+
+    if (!checkbox || !inputA || !inputB) return;
+
     checkbox.addEventListener("change", () => {
-      checkboxes[key] = checkbox.checked;
-      chrome.runtime.sendMessage({
-        addHeader: checkbox.checked && key === "checkbox1",
-      });
+      checkboxes[key] = {
+        checked: checkbox.checked,
+        inputs: [inputA.value, inputB.value],
+      };
       chrome.storage.local.set({ checkboxes });
+    });
+
+    [inputA, inputB].forEach((input, inputIndex) => {
+      input.addEventListener("input", () => {
+        if (!checkboxes[key]) {
+          checkboxes[key] = { checked: checkbox.checked, inputs: ["", ""] };
+        }
+        checkboxes[key].inputs[inputIndex] = input.value;
+        chrome.storage.local.set({ checkboxes });
+      });
     });
   });
 });
